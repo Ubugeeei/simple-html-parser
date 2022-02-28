866f9697fa3e05f35c446cc95aabd02b0063e77a
Ubugeeei <ubuge1122@gamil.com>
2022-03-01 05:28:01 +0900

    implements tag parser

---
+++ src/parser/tag.rs
@@ -0,0 +1,34 @@
+use super::attribute::attributes;
+use crate::interfaces::html_element::AttrMap;
+use combine::error::ParseError;
+use combine::parser::char::{char, letter};
+use combine::{between, many1, Parser, Stream};
+use combine::{
+    many,
+    parser::char::{newline, space},
+};
+
+pub fn open_tag<Input>() -> impl Parser<Input, Output = (String, AttrMap)>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    let open_tag_name = many1::<String, _, _>(letter());
+    let open_tag_content = (
+        open_tag_name,
+        many::<String, _, _>(space().or(newline())),
+        attributes(),
+    )
+        .map(|v: (String, _, AttrMap)| (v.0, v.2));
+    between(char('<'), char('>'), open_tag_content)
+}
+
+pub fn close_tag<Input>() -> impl Parser<Input, Output = String>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    let close_tag_name = many1::<String, _, _>(letter());
+    let close_tag_content = (char('/'), close_tag_name).map(|v| v.1);
+    between(char('<'), char('>'), close_tag_content)
+}
